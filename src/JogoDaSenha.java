49dbe758d0212c6ec4f75c1326ef2c67e8193427
LeandroVC <83590684+leandroV201@users.noreply.github.com>
2023-10-03 19:18:24 -0300

    resolvendo bugs
    usuario não pode mais repetir numeros

--- src/JogoDaSenha.java
+++ src/JogoDaSenha.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 
 public class JogoDaSenha {
+    List<Integer> numeros_usados = new ArrayList<Integer>();
     int x = 0;
+    int s =0;
     int index = 0;
     List<Integer> lista = new ArrayList<Integer>();
+    List<Integer> cache = new ArrayList<Integer>();
     void getNumbers(){
         //classe que interage com o usuario atraves do scanner
         Scanner scanner = new Scanner(System.in);
@@ -16,9 +19,112 @@ public class JogoDaSenha {
         int z = scanner.nextInt();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        fazerTentativa(x, y, z, a, b);
+        TestNumeroRept(x, y, z, a, b);
     }
-    void fazerTentativa(int a, int b, int c, int d, int e) {
+    void TestNumeroRept(int a,int b,int c,int d,int e){
+        cache.removeAll(cache);
+        cache.add(a);
+        cache.add(b);
+        cache.add(c);
+        cache.add(d);
+        cache.add(e);
+        s=0;
+        int s2 = 0;
+        if(numeros_usados.contains(a) == false){
+            s2=0;
+            for(int l =0; l<5;l++){
+                if(a!=cache.get(l)){
+                    s2++;
+                }
+            }
+            if(s2 == 4){
+                s++;
+            }
+            else{
+                System.out.println("Algum numero repetido verifique");
+                getNumbers();
+            }
+        
+        }
+        if(numeros_usados.contains(b) == false){
+            s2=0;
+             for(int l =0; l<5;l++){
+                if(b!=cache.get(l)){
+                    s2++;
+                }
+            }
+            if(s2 == 4){
+                s++;
+            }
+            else{
+                System.out.println("Algum numero repetido verifique");
+                getNumbers();
+            }
+            
+        }
+        if(numeros_usados.contains(c) == false){
+            s2=0;
+             for(int l =0; l<5;l++){
+                if(c!=cache.get(l)){
+                    s2++;
+                }
+            }
+            if(s2 == 4){
+                s++;
+            }
+            else{
+                System.out.println("Algum numero repetido verifique");
+                getNumbers();
+            }
+            
+        }
+        if(numeros_usados.contains(d) == false){
+            s2=0;
+             for(int l =0; l<5;l++){
+                if(d!=cache.get(l)){
+                    s2++;
+                }
+            }
+            if(s2 == 4){
+                s++;
+            }
+            else{
+                System.out.println("Algum numero repetido verifique");
+                getNumbers();
+            }
+            
+        }
+        if(numeros_usados.contains(e) == false){
+            s2=0;
+             for(int l =0; l<5;l++){
+                if(e!=cache.get(l)){
+                    s2++;
+                }
+            }
+            if(s2 == 4){
+                s++;
+            }
+            else{
+                System.out.println("Algum numero repetido verifique");
+                getNumbers();
+            }  
+        }
+        if(s == 5){
+            numeros_usados.add(a);
+            numeros_usados.add(b);
+            numeros_usados.add(c);
+            numeros_usados.add(d);
+            numeros_usados.add(e);
+            fazerTentativa(a, b, c, d, e);
+        }
+        else{
+            System.out.println("Algum numero repetido verifique a lista e tente novamente ");
+            System.out.println(numeros_usados);
+            getNumbers();
+        }
+        
+    }
+    void fazerTentativa(int a, int b, int c, int d, int e)   {
         //classe que cria a lista e verifica os resultados, alem de criar um laço com a getnumbers
         index +=5;
         if(lista.isEmpty() == true){
@@ -34,6 +140,7 @@ public class JogoDaSenha {
     }
         
     if (lista.contains(a)){
+
         x++;        
     }
     if (lista.contains(b)){
@@ -71,7 +178,7 @@ public class JogoDaSenha {
     void getSenha(){
         //Retorna o andamento do jogo
         if(x==5){
-            System.out.println("VOCÊ ACERTOU TODOS, PARABÉNS");
+            System.out.println("VOCÊ ACERTOU, PARABÉNS");
             System.out.println(lista);
             getContadorPesquisa();
         }
